c77dff02d80772106f08d797ab3710d2cf1268d0
Martin Katev <amaranthin@gmail.com>
2025-07-03 14:07:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoGitProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");        
+        }
+    }
+}
