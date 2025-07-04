da73cd227cdf858e73c709ff3d8f78eea10e57c4
Martin Katev <amaranthin@gmail.com>
2025-07-04 13:53:01 +0300

    add person class + add comment in programs

---
+++ Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGitProject
+{
+    internal class Person
+    {
+        //katev01
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }   
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace DemoGitProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");        
+            Console.WriteLine("Hello");    
+            
+            //Comment Change 01 ... in branch katev 01
         }
     }
 }
